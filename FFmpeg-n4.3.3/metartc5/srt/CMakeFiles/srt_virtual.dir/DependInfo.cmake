f67ee6fdaa949c9aaa289ef58f6ab01a575837e4
chengyouliang <chengyouliang@qq.com>
2022-09-30 17:07:31 -0700

    support srt

---
+++ FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/DependInfo.cmake
@@ -0,0 +1,120 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/haicrypt/cryspr-openssl.c" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/haicrypt/cryspr-openssl.c.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/haicrypt/cryspr.c" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/haicrypt/cryspr.c.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/haicrypt/hcrypt.c" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/haicrypt/hcrypt.c.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/haicrypt/hcrypt_ctx_rx.c" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/haicrypt/hcrypt_ctx_rx.c.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/haicrypt/hcrypt_ctx_tx.c" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/haicrypt/hcrypt_ctx_tx.c.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/haicrypt/hcrypt_rx.c" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/haicrypt/hcrypt_rx.c.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/haicrypt/hcrypt_sa.c" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/haicrypt/hcrypt_sa.c.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/haicrypt/hcrypt_tx.c" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/haicrypt/hcrypt_tx.c.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/haicrypt/hcrypt_xpt_srt.c" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/haicrypt/hcrypt_xpt_srt.c.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/srt_compat.c" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/srt_compat.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CRYSPR2"
+  "ENABLE_MONOTONIC_CLOCK=1"
+  "ENABLE_NEW_RCVBUFFER=1"
+  "ENABLE_SOCK_CLOEXEC=1"
+  "HAI_ENABLE_SRT=1"
+  "HAI_PATCH=1"
+  "HAVE_INET_PTON=1"
+  "HAVE_PTHREAD_GETNAME_NP=1"
+  "HAVE_PTHREAD_SETNAME_NP=1"
+  "LINUX=1"
+  "NDEBUG"
+  "SRT_DYNAMIC"
+  "SRT_ENABLE_APP_READER"
+  "SRT_ENABLE_BINDTODEVICE"
+  "SRT_ENABLE_CLOSE_SYNCH"
+  "SRT_ENABLE_ENCRYPTION"
+  "SRT_EXPORTS"
+  "SRT_VERSION=\"1.5.0\""
+  "USE_OPENSSL=1"
+  "_GNU_SOURCE"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "common"
+  "srtcore"
+  "haicrypt"
+  "."
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/haicrypt/haicrypt_log.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/haicrypt/haicrypt_log.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/api.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/api.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/buffer.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/buffer.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/buffer_rcv.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/buffer_rcv.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/cache.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/cache.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/channel.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/channel.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/common.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/common.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/congctl.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/congctl.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/core.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/core.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/crypto.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/crypto.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/epoll.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/epoll.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/fec.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/fec.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/handshake.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/handshake.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/list.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/list.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/logger_default.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/logger_default.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/logger_defs.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/logger_defs.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/md5.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/md5.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/packet.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/packet.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/packetfilter.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/packetfilter.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/queue.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/queue.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/socketconfig.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/socketconfig.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/srt_c_api.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/srt_c_api.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/strerror_defs.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/strerror_defs.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/sync.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/sync.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/sync_posix.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/sync_posix.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/tsbpd_time.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/tsbpd_time.cpp.o"
+  "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/srtcore/window.cpp" "/home/youliang/code/media/ffmpeg-rtc/FFmpeg-n4.3.3/metartc5/srt/CMakeFiles/srt_virtual.dir/srtcore/window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CRYSPR2"
+  "ENABLE_MONOTONIC_CLOCK=1"
+  "ENABLE_NEW_RCVBUFFER=1"
+  "ENABLE_SOCK_CLOEXEC=1"
+  "HAI_ENABLE_SRT=1"
+  "HAI_PATCH=1"
+  "HAVE_INET_PTON=1"
+  "HAVE_PTHREAD_GETNAME_NP=1"
+  "HAVE_PTHREAD_SETNAME_NP=1"
+  "LINUX=1"
+  "NDEBUG"
+  "SRT_DYNAMIC"
+  "SRT_ENABLE_APP_READER"
+  "SRT_ENABLE_BINDTODEVICE"
+  "SRT_ENABLE_CLOSE_SYNCH"
+  "SRT_ENABLE_ENCRYPTION"
+  "SRT_EXPORTS"
+  "SRT_VERSION=\"1.5.0\""
+  "USE_OPENSSL=1"
+  "_GNU_SOURCE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "common"
+  "srtcore"
+  "haicrypt"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
